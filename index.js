4d8e5f1dbaa6e732a2aee7e92728c23afea26132
Kieran Burke <kieran314237@hotmail.co.uk>
2020-07-29 09:17:16 +0900

    [fix] joining matches [add] eslint config

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-
 function search(string, keywords, range = 5) {
-  if (typeof string !== "string") throw new TypeError("Please provide a string");
-  var results = []
-  var re = new RegExp(keywords.join("|"), 'gi');
-  while ((match = re.exec(string)) !== null) {
-    for (var i = 0; i < match.length; i++) {
-      results.push({start: Math.max(match.index - range, 0), end: Math.min(match.index + match[i].length + range, string.length), highlights: [{start: match.index, length: match[i].length}]})
+  if (typeof string !== 'string') throw new TypeError('Please provide a string');
+  let match;
+  const results = [];
+  const re = new RegExp(keywords.filter((k) => k).join('|'), 'gi');
+  match = re.exec(string);
+  while (match !== null) {
+    for (let i = 0; i < match.length; i += 1) {
+      results.push({
+        start: Math.max(match.index - range, 0),
+        end: Math.min(match.index + match[i].length + range, string.length),
+        highlights: [{ start: match.index, length: match[i].length }],
+      });
     }
+    match = re.exec(string);
   }
-  results.sort((a, b) => a[0] - b[0])
-  for (var i = 0; i < results.length - 1; i++) {
-    // if the beginning on the next match is within range of the end of the previous match, join together
+  results.sort((a, b) => a[0] - b[0]);
+  for (let i = 0; i < results.length - 1; i += 1) {
+    /* if the beginning on the next match is within
+     * range of the end of the previous match, join together
+     */
     if (results[i + 1].start <= results[i].end) {
       // only update the "end" position of the match if its greater
       if (results[i + 1].end > results[i].end) {
-        results[i].end = results[i + 1].end
-        results[i].highlights.push(results[i + 1].highlights[0])
+        results[i].end = results[i + 1].end;
+        results[i].highlights.push(results[i + 1].highlights[0]);
       }
       // remove from array
-      results.splice(i+1, 1)
+      results.splice(i + 1, 1);
+      i -= 1;
     }
   }
-  return results
-};
+  return results;
+}
 
 function defaultHighlightFunction(text) {
-  return '<span style="background-color: yellow">' + text + '</span>'
+  return `<span style="background-color: yellow">${text}</span>`;
 }
 
 module.exports.highlightedSearch = function highlightedSearch(
@@ -33,27 +42,38 @@ module.exports.highlightedSearch = function highlightedSearch(
   keywords,
   range = 5,
   matchConnector = ' ...',
-  highlightFunction = defaultHighlightFunction
+  highlightFunction = defaultHighlightFunction,
 ) {
-  var matches = search(string, keywords, range)
-  var result = [];
-  for (var i = 0; i < matches.length; i++) {
-    var matchString = matches[i].start === 0 ? [] : [matchConnector.trimStart()]
-    matchString.push(string.substr(matches[i].start, matches[i].highlights[0].start - matches[i].start))
-    for (var j = 0; j < matches[i].highlights.length; j++) {
-      var currentMatch = matches[i].highlights[j]
-      matchString.push(highlightFunction(string.substr(currentMatch.start, currentMatch.length)))
+  const matches = search(string, keywords, range);
+  const result = [];
+  for (let i = 0; i < matches.length; i += 1) {
+    const matchString = matches[i].start === 0 ? [] : [matchConnector.trimStart()];
+    matchString.push(
+      string.substr(
+        matches[i].start,
+        matches[i].highlights[0].start - matches[i].start,
+      ),
+    );
+    for (let j = 0; j < matches[i].highlights.length; j += 1) {
+      const currentMatch = matches[i].highlights[j];
+      matchString.push(highlightFunction(string.substr(currentMatch.start, currentMatch.length)));
       if (j !== matches[i].highlights.length - 1) {
-        var nextMatch = matches[i].highlights[j + 1]
-        var nextString = string.substr(currentMatch.start + currentMatch.length, nextMatch.start - (currentMatch.start + currentMatch.length))
-        matchString.push(nextString.trim() + matchConnector)
+        const nextMatch = matches[i].highlights[j + 1];
+        const nextString = string.substr(
+          currentMatch.start + currentMatch.length,
+          nextMatch.start - (currentMatch.start + currentMatch.length),
+        );
+        matchString.push(nextString);
       }
     }
-    var lastHighlight = matches[i].highlights[matches[i].highlights.length-1]
-    matchString.push(string.substr(lastHighlight.start + lastHighlight.length, matches[i].end - (lastHighlight.start + lastHighlight.length)))
-    result.push(matchString)
+    const lastHighlight = matches[i].highlights[matches[i].highlights.length - 1];
+    matchString.push(string.substr(
+      lastHighlight.start + lastHighlight.length,
+      matches[i].end - (lastHighlight.start + lastHighlight.length),
+    ));
+    result.push(matchString);
   }
-  return result
-}
+  return result;
+};
 
-module.exports.search = search
+module.exports.search = search;
--- test.js
+++ test.js
@@ -1,38 +1,39 @@
 const test = require('ava');
-const {search, highlightedSearch } = require('./index')
+const { search, highlightedSearch } = require('./index');
 
-test('matches are separated', t => {
-  const result = search('Hello world next world', ['world'], 1)
-  t.is(result.length, 2)
-  t.is(result[0].end, 12)
-  t.is(result[0].start, 5)
-  t.is(result[0].highlights.length, 1)
-  t.is(result[1].end, 22)
-  t.is(result[1].start, 16)
-  t.is(result[1].highlights.length, 1)
+test('matches are separated', (t) => {
+  const result = search('Hello world next world', ['world'], 1);
+  t.is(result.length, 2);
+  t.is(result[0].end, 12);
+  t.is(result[0].start, 5);
+  t.is(result[0].highlights.length, 1);
+  t.is(result[1].end, 22);
+  t.is(result[1].start, 16);
+  t.is(result[1].highlights.length, 1);
 });
-test('matches are aggregated', t => {
-  const result = search('Hello world next world', ['world'], 10)
-  t.is(result.length, 1)
-  t.is(result[0].end, 22)
-  t.is(result[0].start, 0)
-  t.is(result[0].highlights.length, 2)
+test('matches are aggregated', (t) => {
+  const result = search('Hello world next world', ['world'], 10);
+  t.is(result.length, 1);
+  t.is(result[0].end, 22);
+  t.is(result[0].start, 0);
+  t.is(result[0].highlights.length, 2);
 });
-test('highlighted search', t => {
-  const result = highlightedSearch('Hello world next world', ['world'], 10)
+test('highlighted search', (t) => {
+  const result = highlightedSearch('Hello world next world', ['world'], 10);
   const expected = [
     [
       'Hello ',
       '<span style="background-color: yellow">world</span>',
-      'next ...',
+      ' next ',
       '<span style="background-color: yellow">world</span>',
       '',
     ],
-  ]
-  t.deepEqual(result, expected)
+  ];
+  t.deepEqual(result, expected);
 });
-test('long highlighted search', t => {
-  const result = highlightedSearch('Tri-tip fatback ham pork belly doner landjaeger biltong spare ribs cow strip steak turducken pork chop andouille. Frankfurter jerky sausage corned beef. Strip steak salami kielbasa t-bone brisket pork belly landjaeger venison drumstick. Bacon sausage turkey ball tip andouille, bresaola ribeye strip steak jerky ham hock meatball chicken porchetta. Ribeye pastrami tenderloin shank. Fatback porchetta pastrami swine, bresaola alcatra shank tenderloin venison biltong beef ribs.', ['Bacon', 'swine'], 10)
+const baconIpsum = 'Tri-tip fatback ham pork belly doner landjaeger biltong spare ribs cow strip steak turducken pork chop andouille. Frankfurter jerky sausage corned beef. Strip steak salami kielbasa t-bone brisket pork belly landjaeger venison drumstick. Bacon sausage turkey ball tip andouille, bresaola ribeye strip steak jerky ham hock meatball chicken porchetta. Ribeye pastrami tenderloin shank. Fatback porchetta pastrami swine, bresaola alcatra shank tenderloin venison biltong beef ribs.';
+test('long highlighted search', (t) => {
+  const result = highlightedSearch(baconIpsum, ['Bacon', 'swine'], 10);
   const expected = [
     [
       '...',
@@ -46,10 +47,12 @@ test('long highlighted search', t => {
       '<span style="background-color: yellow">swine</span>',
       ', bresaola',
     ],
-  ]
-  t.deepEqual(result, expected)
+  ];
+  t.deepEqual(result, expected);
 });
 
-
-
-
+test('many matches highlighted search', (t) => {
+  const result = search('Hello my name is bob and I live down a street with many people', ['e'], 10);
+  t.is(result.length, 1);
+  t.is(result[0].highlights.length, 6);
+});
